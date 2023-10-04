971ca2f48df701931640fcfac022b180974a59c9
Richard <legalize@xmission.com>
2023-10-03 19:28:55 -0600

    Fallback to pregenerated sources when flex/bison not found

---
+++ src/asm/generated/asm.yacc.hpp
@@ -0,0 +1,88 @@
+/* A Bison parser, made by GNU Bison 2.4.2.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2006, 2009-2010 Free Software
+   Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     INDEX = 258,
+     FCB = 259,
+     FDB = 260,
+     DFB = 261,
+     RMB = 262,
+     ZMB = 263,
+     FCC = 264,
+     END = 265,
+     ORG = 266,
+     DFL = 267,
+     DWL = 268,
+     NAM = 269,
+     DWM = 270,
+     DFS = 271,
+     OPT = 272,
+     EQU = 273,
+     BSZ = 274,
+     PAGE = 275,
+     FILL = 276,
+     INST = 277,
+     NUMBER = 278,
+     ID = 279,
+     TEXT = 280
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1685 of yacc.c  */
+#line 76 "C:/Code/legalize/hippy/hippy/src/asm/asm.y"
+ int ival; void* pse; 
+
+
+/* Line 1685 of yacc.c  */
+#line 80 "C:/Code/legalize/hippy/build/asm/asm.yacc.hpp"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
