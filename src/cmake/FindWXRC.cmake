db9ec890f1ee7c467ee82584a9dc6c6b2d650937
Richard <legalize@xmission.com>
2023-10-14 18:08:39 -0600

    Correct package name

--- src/cmake/FindWXRC.cmake
+++ src/cmake/FindWXRC.cmake
@@ -4,4 +4,4 @@ find_program(WXRC_COMMAND
     wxrc
     HINTS "${wxWidgets_ROOT_DIR}/tools/wxwidgets"
 )
-find_package_handle_standard_args(XRC REQUIRED_VARS WXRC_COMMAND)
+find_package_handle_standard_args(WXRC REQUIRED_VARS WXRC_COMMAND)
